978975909a1fa00ed7860351d082654660774ca6
avihu.elbaz <c>
2022-06-15 14:43:03 +0300

    1.2

--- Bill_Share/main.py
+++ Bill_Share/main.py
@@ -1,3 +1,7 @@
+import webbrowser
+from fpdf import *
+import time
+
 class Bill:
     """
     Object that contains data about a bill such as total amount and period
@@ -25,13 +29,33 @@ class PdfReport:
     """
     def __init__(self, filename):
         self.filename=filename
-        def generate(self,flatmate1, flatmate2, bill):
-            pass
-
+    def generate(self,flatmate1,flatmate2,bill):
+       flatmate1_pay=str(round(flatmate1.pays(bill,flatmate2),2))
+       flatmate2_pay=str(round(flatmate2.pays(bill,flatmate1),2))
+       pdf=FPDF(orientation='P',unit='pt',format='A4')
+       pdf.add_page()
+       #Insert title
+       pdf.set_font(family='Times',size=24,style='B')
+       pdf.cell(w=0,h=80,txt="Flatmates Bill",border=1,align="C",ln=1)
+       #Insert Period label and value
+       pdf.set_font(family='Times',size=12)
+       pdf.cell(w=100,h=40,txt="Period:",border=1)
+       pdf.cell(w=150,h=40,txt=bill.period,border=1,ln=1)
+       #Insert name and due amount of the first flatmate
+       pdf.cell(w=100,h=40,txt=flatmate1.name,border=1)
+       pdf.cell(w=150,h=40,txt=flatmate1_pay,border=1,ln=1)
+       pdf.cell(w=100,h=40,txt=flatmate2.name,border=1)       
+       pdf.cell(w=150,h=40,txt=flatmate2_pay,border=1)    
+       pdf.output(self.filename)
+       webbrowser.open(self.filename)
+       time.sleep(10)
 
 bill = Bill(120, "March 2021")
 john = flatmate("John", 20)
-marry = flatmate("Marry", 10)
+marry = flatmate("Marry", 25)
 
 print (john.pays(bill,marry))
 print (marry.pays(bill,john))
+
+pdf_report=PdfReport('Report.pdf')
+pdf_report.generate(john,marry,bill)
